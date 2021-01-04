bd4e18158a2850beeaee43a29b832300f5f6ddee
Nandan Wewhare <nandanwewhare25@gmail.com>
2021-01-04 14:14:15 +0530

    minor changes

--- src/City.js
+++ src/City.js
@@ -4,19 +4,21 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function City(props) {
   return (
-    <div class="card city__card">
-      <img src={props.image} class="card-img-top" alt="..." />
-      <div class="card-body card__body">
-        <div className="general__details">
-          <h5 class="card-title">{props.title}</h5>
-          <h5 className="city__price">{props.price}</h5>
+
+      <div class="card city__card">
+        <img src={props.image} class="card-img-top" alt="..." />
+        <div class="card-body card__body">
+          <div className="general__details">
+            <h5 class="card-title">{props.title}</h5>
+            <h5 className="city__price">{props.price}</h5>
+          </div>
+          <p class="card-text city__description">{props.description}</p>
+          <a href="#" class="btn btn-outline-danger">
+            Not Interested
+          </a>
         </div>
-        <p class="card-text city__description">{props.description}</p>
-        <a href="#" class="btn btn-outline-danger">
-          Not Interested
-        </a>
       </div>
-    </div>
+
   );
 }
 
