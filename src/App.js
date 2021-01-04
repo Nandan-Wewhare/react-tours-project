18d169152bb782989ada426ada2b92df9c39eda0
Nandan Wewhare <nandanwewhare25@gmail.com>
2021-01-04 15:23:43 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,68 +1,57 @@
 import Heading from "./Heading";
 import City from "./City";
+const cities = [
+  {
+    image:
+      "https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg",
+    title: "Best Of Paris In 7 Days Tour",
+    price: "$2599",
+    description:
+      "Paris is synonymous with the finest things that culture can offer — in art, fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves guide will immerse you in the very best of the City of Light: the masterpiece-packed Louvre and Orsay museums, resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and   extravagant Palace of Versailles. You'll also enjoy guided    neighborhood walks through the city historic heart as well as    quieter moments to slow down and savor the city intimate cafés    colorful markets and joie de vivre. Join us for the Best of Paris in 7 Days!",
+  },
+  {
+    image:
+      "https://dl.airtable.com/.attachments/6c24084000a3777064c5200a8c2ae931/04081a3e/ireland.jpeg",
+
+    title: "Best Of Ireland In 14 Days Tour",
+    price: "$3599",
+    description:
+      "Rick Steves' Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites, charming towns, music-filled pubs, and seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!",
+  },
+  {
+    image:
+      "https://dl.airtable.com/.attachments/27f6cbfe631e303f98b97e9dafacf25b/6bbe2a07/vienna.jpeg",
+    title: "Best Of Salzburg & Vienna In 8 Days Tour",
+    price: "$2599",
+    description:
+      "Let's go where classical music, towering castles, and the-hills-are-alive ,  scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age.   Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich,   Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna —  with cozy villages and alpine vistas all along the way. Join us for the Best of Munich,    Salzburg & Vienna in 8 Days!",
+  },
+  {
+    image:
+      "https://dl.airtable.com/.attachments/3efa7aa402d49c12b8769c581a96af42/d5b641e3/italy.jpeg",
+
+    title: "Best Of Rome In 7 Days Tour",
+    price: "$2899",
+    description:
+      "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!",
+  },
+];
+
 function App() {
   return (
     <>
       <Heading />
-      <City
-        image={
-          "https://dl.airtable.com/.attachments/a0cd0702c443f31526267f38ea5314a1/2447eb7a/paris.jpg"
-        }
-        title="Best Of Paris In 7 Days Tour"
-        price="$2599"
-        description="Paris is synonymous with the finest things that culture can offer — in
-        art, fashion, food, literature, and ideas. On this tour, your
-        Paris-savvy Rick Steves guide will immerse you in the very best of the
-        City of Light: the masterpiece-packed Louvre and Orsay museums,
-        resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and
-        extravagant Palace of Versailles. You'll also enjoy guided
-        neighborhood walks through the city historic heart as well as
-        quieter moments to slow down and savor the city intimate cafés
-        colorful markets and joie de vivre. Join us for the Best of Paris in
-        7 Days!"
-      />
-      <City
-        image={
-          "https://dl.airtable.com/.attachments/6c24084000a3777064c5200a8c2ae931/04081a3e/ireland.jpeg"
-        }
-        title="Best Of Ireland In 14 Days Tour"
-        price="$3599"
-        description="Rick Steves' Best of Ireland tour kicks off with the best of Dublin, 
-        followed by Ireland's must-see historical sites, charming towns, music-filled pubs, 
-        and seaside getaways — including Kinsale, the Dingle Peninsula, the Cliffs of Moher, 
-        the Aran Islands, Galway, Connemara, Giant's Causeway, and the compelling city of Belfast. 
-        All along the way, Rick's guides will share their stories to draw you in to the Emerald Isle, 
-        and the friendliness of the people will surely steal your heart. Join us for the Best of Ireland in 14 Days!"
-      />
-      <City
-        image={
-          "https://dl.airtable.com/.attachments/27f6cbfe631e303f98b97e9dafacf25b/6bbe2a07/vienna.jpeg"
-        }
-        title="Best Of Salzburg & Vienna In 8 Days Tour"
-        price="$2599"
-        description="Let's go where classical music, towering castles, and the-hills-are-alive 
-        scenery welcome you to the gemütlichkeit of Bavaria and opulence of Austria's Golden Age. 
-        Your Rick Steves guide will bring this region's rich history and culture to life in festive Munich,
-         Baroque Salzburg, sparkling Lake Hallstatt, monastic Melk, the blue Danube, and royal Vienna — 
-         with cozy villages and alpine vistas all along the way. Join us for the Best of Munich, 
-         Salzburg & Vienna in 8 Days!"
-      />
-      <City
-        image={
-          "https://dl.airtable.com/.attachments/3efa7aa402d49c12b8769c581a96af42/d5b641e3/italy.jpeg"
-        }
-        title="Best Of Rome In 7 Days Tour"
-        price="$2899"
-        description="Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient Rome's Colosseum, Forum, Pantheon, and nearby Ostia Antica. From the Renaissance and Baroque eras, you'll marvel at St. Peter's Basilica, the Vatican Museums, Sistine Chapel, and Borghese Gallery. You'll also enjoy today's Rome, with neighborhood walking tours, memorable restaurants, and time to explore on your own. Join us for the Best of Rome in 7 Days!"
-      />
-      <City
-        image={
-          "https://dl.airtable.com/.attachments/3feee7a93af0f4f809312132090c9a80/58e3e8ec/poland.jpeg"
-        }
-        title="Best Of Poland In 10 Days Tour"
-        price="$3099"
-        description="Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, you'll experience mighty Malbork castle, the cobbly-cute village of Toruń, Poland's contemporary capital of Warsaw, the spiritual Jasna Góra Monastery, and charming Kraków — Poland's finest city. In this land of surprises — so trendy and hip, yet steeped in history — there's so much to discover. Join us for the Best of Poland in 10 Days!"
-      />
+      {cities.map((city) => {
+        return (
+          <City
+            key={city.image.slice(37, 42)}
+            image={city.image}
+            title={city.title}
+            price={city.price}
+            description={city.description}
+          />
+        );
+      })}
     </>
   );
 }
--- src/City.js
+++ src/City.js
@@ -4,21 +4,23 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function City(props) {
   return (
-
-      <div class="card city__card">
-        <img src={props.image} class="card-img-top" alt="..." />
-        <div class="card-body card__body">
-          <div className="general__details">
-            <h5 class="card-title">{props.title}</h5>
-            <h5 className="city__price">{props.price}</h5>
-          </div>
-          <p class="card-text city__description">{props.description}</p>
-          <a href="#" class="btn btn-outline-danger">
-            Not Interested
-          </a>
+    <div className="card city__card">
+      <img src={props.image} className="card-img-top" alt="..." />
+      <div className="card-body card__body">
+        <div className="general__details">
+          <h5 className="card-title">{props.title}</h5>
+          <h5 className="city__price">{props.price}</h5>
         </div>
+        <p className="card-text city__description">{props.description}</p>
+        <a
+          href="https://www.makemytrip.com/"
+          className="btn btn-block btn-success"
+          target="__blank"
+        >
+          Plan a Trip!
+        </a>
       </div>
-
+    </div>
   );
 }
 
